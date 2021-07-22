8f0dcee3d7abf975d995cb193c3c3ba2c7bd8dbb
T.Lai <timlai1208@gmail.com>
2021-07-22 17:00:00 +0800

    backend environment setup

--- callunch_backend/callunch/admin.py
+++ callunch_backend/callunch/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import LunchList
 # Register your models here.
+class CallLunchAdmin(admin.ModelAdmin):
+    list = ('name', 'order', 'completed')
+
+admin.site.register(LunchList, CallLunchAdmin)
